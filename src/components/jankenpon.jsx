ddf7f5bff1f323006241878b28e247baa9437a73
henriknestor <henrik.gabriel.nestor@gmail.com>
2024-01-04 18:15:53 +0100

    keep on tryin
    Försöker förstå hur jag ska få knapparna att funka

--- src/components/jankenpon.jsx
+++ src/components/jankenpon.jsx
@@ -1,20 +1,48 @@
 import Button from "./Button";
+import { useState } from "react";
 
-const Janken = () => {
-    const sten = alert("Sten");
+const Janken = ({rand}) => {
+    const [num, setNum] = useState(1);
 
-    // Äh, jag fattar ingenting :(
+    const genRandom = () => {
+        rand = Math.floor(Math.random() *10) +1;
+        setNum(rand);
+    }
+
+let choice = ""
+
+    const sten = () => {
+        choice = "Sten";
+    }
+
+    const sax = () => {
+        choice = "Sax";
+    }
+
+    const pase = () => {
+        choice = "Påse";
+    }
+
+    // Funkar inte :(
 
     return (
-        <>
-            <div className="container">
+        <div className="container">
+
+            <p>Randomizer</p>
+            {num === 7 ? (
+                <h2>You won!</h2>
+            ) : (
+            <button onClick={genRandom}>Randomize</button>
+            )}
+            <h2>Your number: {num}</h2>
             <h1>Jankenpon</h1>
-            <p>Let´s play janken</p>
-            <Button onClick={sten}>Sten</Button>
-            <button onClick={alert("Sax")}>Sax</button>
-            <Button>Påse</Button>
-            </div>
-        </>
+            <p>Let´s play Janken</p>
+            <button onClick={sten}>Sten</button>
+            <button onClick={sax}>Sax</button>
+            <button onClick={pase}>Påse</button>
+            <h2>Your choice: {choice}</h2>
+
+        </div>
     );
 };
 
