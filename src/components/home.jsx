e1af2145e706becf837549e6c4262a058a015988
henriknestor <henrik.gabriel.nestor@gmail.com>
2024-01-04 17:58:00 +0100

    Tryin to get the buttons to work
    Fattar inte hur man får knapparna att reagera på att man klickar på dem :/

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,15 +1,18 @@
 import Button from "./Button";
+import {Link} from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-    const navigate = useNavigate();
+    const navigate = useNavigate("./../janken");
+    // Funkar inte :(
 
     return (
         <>
             <div className="container">
             <h1>Home</h1>
             <p>Now you are home</p>
-            <Button onClick={navigate}>Najs</Button>
+            <Button onClick={navigate}>Play Janken</Button>
+            <Link className="App-link" to="./../janken">Play Janken</Link>         
             </div>
         </>
     );
--- src/components/jankenpon.jsx
+++ src/components/jankenpon.jsx
@@ -1,13 +1,17 @@
 import Button from "./Button";
 
 const Janken = () => {
+    const sten = alert("Sten");
+
+    // Äh, jag fattar ingenting :(
+
     return (
         <>
             <div className="container">
             <h1>Jankenpon</h1>
             <p>Let´s play janken</p>
-            <Button>Sten</Button>
-            <Button>Sax</Button>
+            <Button onClick={sten}>Sten</Button>
+            <button onClick={alert("Sax")}>Sax</button>
             <Button>Påse</Button>
             </div>
         </>
